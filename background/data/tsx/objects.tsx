f4162a0309798ff3b69fa0bdd4b0e48b1e13183a
kysMIRACLE <156891752+kysMIRACLE@users.noreply.github.com>
2024-04-19 23:22:16 +0500

    Add files via upload

---
+++ background/data/tsx/objects.tsx
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="objects" tilewidth="666" tileheight="524" tilecount="9" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="100" height="100" source="../../graphics/objects/block.png"/>
+ </tile>
+ <tile id="1">
+  <image width="100" height="100" source="../../graphics/objects/chair.png"/>
+ </tile>
+ <tile id="2">
+  <image width="476" height="524" source="../../graphics/objects/chandelier.png"/>
+ </tile>
+ <tile id="3">
+  <image width="100" height="100" source="../../graphics/objects/computerr.png"/>
+ </tile>
+ <tile id="4">
+  <image width="100" height="100" source="../../graphics/objects/red block.png"/>
+ </tile>
+ <tile id="5">
+  <image width="100" height="100" source="../../graphics/objects/stone.png"/>
+ </tile>
+ <tile id="6">
+  <image width="666" height="375" source="../../graphics/objects/things.png"/>
+ </tile>
+ <tile id="7">
+  <image width="30" height="30" source="../../graphics/objects/stair.png"/>
+ </tile>
+ <tile id="8">
+  <image width="30" height="30" source="../../graphics/objects/stair.png"/>
+ </tile>
+</tileset>
