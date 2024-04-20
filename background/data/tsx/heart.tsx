2ed66e55260a8806850b596542484b696506a867
kysMIRACLE <156891752+kysMIRACLE@users.noreply.github.com>
2024-04-20 13:31:10 +0500

    Add files via upload

--- background/data/tsx/heart.tsx
+++ background/data/tsx/heart.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="heart" tilewidth="64" tileheight="64" tilecount="110" columns="11">
- <image source="../../../../Изображения/Снимки экрана/Снимок экрана 2024-04-19 173412.png" width="749" height="651"/>
+ <image source="../../graphics/tilesets/heart.png" width="749" height="651"/>
 </tileset>
---
+++ background/data/tsx/terrain_sets.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="terrain_sets" tilewidth="64" tileheight="64" tilecount="256" columns="16">
+ <image source="../../graphics/tilesets/terrain_sets.png" width="1024" height="1024"/>
+</tileset>
