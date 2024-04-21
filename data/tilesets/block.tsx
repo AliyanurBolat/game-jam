2360e9d2d42ce627d7822951733ee5e2a8f263e7
NurkhanZ <156807161+NurkhanZ@users.noreply.github.com>
2024-04-21 18:04:01 +0600

    Add files via upload

---
+++ data/tilesets/block.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="block" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <image source="block.png" width="100" height="100"/>
+</tileset>
